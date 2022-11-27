88cb16efed24dc2a6a7e6b75ad26954805656340
daviixo <daviidvx93@gmail.com>
2022-11-27 13:37:39 -0600

    "required" added to the form. Image changed

--- js/create.js
+++ js/create.js
@@ -1,68 +1,70 @@
-function sendinfo(){
+function sendinfo() {
 
     try {
-            
+
         var firebaseConfig = {
-        apiKey: "AIzaSyCQMF6gSC6eB-zNstpjjUfjDR7Gfdhpgpk",
-        authDomain: "myrestaurant-158ca.firebaseapp.com",
-        projectId: "myrestaurant-158ca",
-        databaseURL: "https://myrestaurant-158ca-default-rtdb.firebaseio.com/",
-        storageBucket: "myrestaurant-158ca.appspot.com",
-        messagingSenderId: "742839422891",
-        appId: "1:742839422891:web:35e07e3bb2f208242b8cee",
-        measurementId: "G-TN5243W30T"
-    };
+            apiKey: "AIzaSyCQMF6gSC6eB-zNstpjjUfjDR7Gfdhpgpk",
+            authDomain: "myrestaurant-158ca.firebaseapp.com",
+            projectId: "myrestaurant-158ca",
+            databaseURL: "https://myrestaurant-158ca-default-rtdb.firebaseio.com/",
+            storageBucket: "myrestaurant-158ca.appspot.com",
+            messagingSenderId: "742839422891",
+            appId: "1:742839422891:web:35e07e3bb2f208242b8cee",
+            measurementId: "G-TN5243W30T"
+        };
 
-    firebase.initializeApp(firebaseConfig);
+        firebase.initializeApp(firebaseConfig);
 
-    var messagesRef = firebase.database()
-        .ref('Collected Data');
+        var messagesRef = firebase.database()
+            .ref('Collected Data');
 
-    document.getElementById('contactForm')
-        .addEventListener('submit', submitForm);
+        document.getElementById('contactForm')
+            .addEventListener('submit', submitForm);
 
-    function submitForm(e) {
-        e.preventDefault();
+        function submitForm(e) {
 
-        // Get values
-        var name = getInputVal('name');
-        var email = getInputVal('email');
-        var deliveryaddress = getInputVal('deliveryAddress');
-        var phoneNumber = getInputVal('phoneNumber');
-        var paymentForm = getInputVal('paymentForm');
-        var order = getInputVal('order');
-        var quantity = getInputVal('quantity');
-        var comments = getInputVal('comments');
+            e.preventDefault();
 
-        saveMessage(name, email, deliveryaddress, phoneNumber, paymentForm, order, quantity, comments);
-        document.getElementById('contactForm').reset();
+            // Get values
+            var name = getInputVal('name');
+            var email = getInputVal('email');
+            var deliveryaddress = getInputVal('deliveryAddress');
+            var phoneNumber = getInputVal('phoneNumber');
+            var paymentForm = getInputVal('paymentForm');
+            var order = getInputVal('order');
+            var quantity = getInputVal('quantity');
+            var comments = getInputVal('comments');
 
-        alert('Data sent! :D')
+            saveMessage(name, email, deliveryaddress, phoneNumber, paymentForm, order, quantity, comments);
+            document.getElementById('contactForm').reset();
 
-    }
+            alert('Data sent! :D')
 
-    // Function to get form values
-    function getInputVal(id) {
-        return document.getElementById(id).value;
-    }
+        }
 
-    // Save message to firebase
-    function saveMessage(name, email, deliveryaddress, phoneNumber, paymentForm, order, quantity, comments) {
-        var newMessageRef = messagesRef.push();
-        newMessageRef.set({
-            name: name,
-            email: email,
-            deliveryaddress: deliveryaddress,
-            phoneNumber: phoneNumber,
-            paymentForm: paymentForm,
-            order: order,
-            quantity: quantity,
-            comments: comments,
-        });
-    }
-        
+        // Function to get form values
+        function getInputVal(id) {
+            return document.getElementById(id).value;
+        }
+
+        // Save message to firebase
+        function saveMessage(name, email, deliveryaddress, phoneNumber, paymentForm, order, quantity, comments) {
+
+            var newMessageRef = messagesRef.push();
+            newMessageRef.set({
+                name: name,
+                email: email,
+                deliveryaddress: deliveryaddress,
+                phoneNumber: phoneNumber,
+                paymentForm: paymentForm,
+                order: order,
+                quantity: quantity,
+                comments: comments,
+            });
+        }
+
     } catch (error) {
-        
+
         alert('An error occurred: ' + error)
 
     }
